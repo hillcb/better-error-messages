ed50ff5ea25bba33c8defbde4a92565455b4a3dd
Christian Hill <hillcb@uchicago.edu>
2019-04-18 18:09:39 -0500

    remove ignored files

--- lib/rails/extensions/active_record.rb
+++ lib/rails/extensions/active_record.rb
@@ -3,25 +3,5 @@ module ActiveRecord
   
   protected
   
-      def generate_full_message(options = {})
-        keys = [
-          :"full_messages.#{@message}",
-          :'full_messages.format'
-        ]
-        
-        if self.message =~ /^\^/
-          keys.push('{{message}}')
-  
-          options.merge!(:default => self.message[1..-1])
-        else
-          keys.push('%{attribute} %{message}')
-  
-          options.merge!(:default => keys, :message => self.message)        
-        end
-        
-        I18n.translate(keys.shift, options)
-      end
-    end
-  end
 
   # TODO
