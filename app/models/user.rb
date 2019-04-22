65804daf3526d31ca60f8cb4d441496d9419b159
junwen yang <wsklxsx@126.com>
2019-04-22 03:20:25 +0000

    change test case

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    validates_presence_of :name, :message => '^Please set name for user'
     validates_inclusion_of :name, :in => %w( m f )
+    validates_exclusion_of :name, :in => %w( admin administrator )
     validates :name, presence: true,
                      length: { minimum: 1}
     validates :email, presence: true,
--- test.rb
+++ test.rb
@@ -1,3 +1,10 @@
 u = User.new
+u.email = "eamo@df.com"
+u.valid?
+puts u.errors.full_messages
+
+u = User.new
+u.name = "admin"
+u.email = "abc@example.com"
 u.valid?
 puts u.errors.full_messages
