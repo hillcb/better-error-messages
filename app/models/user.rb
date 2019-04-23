005a89acc04df0409957d07088c65e91d0fea66a
junwen yang <wsklxsx@126.com>
2019-04-23 21:08:03 +0000

    create test cases for uniquess

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
     validates_inclusion_of :name, :in => %w( m f )
     validates_exclusion_of :name, :in => %w( admin administrator )
+    validates_uniqueness_of :name, :case_sensitive => true
+    validates_uniqueness_of :email, :case_sensitive => false
     validates_confirmation_of :email
     validates :name, presence: true,
                      length: { minimum: 1}
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,45 @@
-require 'test_helper'
-
+#require 'test_helper'
+#require "test/unit"
+require File.join(File.expand_path(File.dirname(__FILE__)), '../test_helper.rb') 
 class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "inclusion exclusion msg" do
+    u = User.new
+    u.name = "admin"
+    u.email = "abc@example.com"
+    u.valid?
+    msgs = u.errors.full_messages
+    msg0 = "Name is not included in the list you should choose from [m, f]"
+    msg1 = "Name is reserved you should not choose from [admin, administrator]"
+    assert_equal msg0, msgs[0]
+    assert_equal msg1, msgs[1]
+  end
+  test "confirmation msg" do
+    u = User.new
+    u.name = "m"
+    u.email_confirmation = ""
+    u.valid?
+    msgs = u.errors.full_messages
+    msg0 = "Email confirmation doesn't match Email, Email is case sensitive"
+  end
+  test "uniqueness msg" do
+    u = User.find_or_create_by(name: "m", email: "test@gmail.com")
+    assert_equal true, u.valid?
+    u1 = User.create(name: "m", email: "test2@gmail.com")
+    assert_equal false, u1.valid?
+    msgs = u1.errors.full_messages
+    msg0 = "Name has already been taken, Name is case sensitive"
+    assert_equal msg0, msgs[0]
+  end
+  
+  test "uniqueness2 msg" do
+    u = User.find_or_create_by(name: "m", email: "test@gmail.com")
+    u2 = User.create(name: "f", email: "test@gmail.com")
+    assert_equal false, u2.valid?
+    msgs = u2.errors.full_messages
+    msg0 = "Email has already been taken, Email isn't case sensitive"
+    assert_equal msg0, msgs[0]
+  end
 end
